8e669b50f79236b22337014c4f7796bfe7d98a5c
deventw <deven.tw@outlook.com>
2025-05-24 00:23:11 +0800

    minor fix

--- src/PayCode.tsx
+++ src/PayCode.tsx
@@ -1,36 +1,32 @@
-import { useEffect, useRef } from 'react';
-import { update_qrcode } from './generate';
-import { PayCodeProps } from './types';
-
+import React, { useEffect, useRef } from "react";
+import { update_qrcode } from "./generate";
+import { PayCodeProps } from "./types";
 
 export const PayCode = (props: PayCodeProps) => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
-  
+
   const {
     value,
     typeNumber = 0,
     errorCorrectionLevel = "M",
     size = 344,
     consumer = false,
-    logoSrc
+    logoSrc,
   } = props;
 
   useEffect(() => {
     if (canvasRef.current) {
-      update_qrcode({ 
-        text: value, 
-        typeNumber, 
-        errorCorrectionLevel, 
-        size, 
+      update_qrcode({
+        text: value,
+        typeNumber,
+        errorCorrectionLevel,
+        size,
         consumer,
         imageSrc: logoSrc,
-        canvas: canvasRef.current 
+        canvas: canvasRef.current,
       });
     }
   }, [value, typeNumber, errorCorrectionLevel, size, consumer]);
 
-  return (
-    <canvas ref={canvasRef} width={size} height={size}></canvas>
-  );
+  return <canvas ref={canvasRef} width={size} height={size}></canvas>;
 };
-
